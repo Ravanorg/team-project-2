ff1251c73b841afb98a408fb83331dc8e64a4686
CharithLahiru <wlcharithlahiru@gmail.com>
2023-03-23 18:11:35 +0530

    design UI

---
+++ src/main/java/lk/ijse/dep10/mc/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package lk.ijse.dep10.mc.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class EmployeeController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewEmployee;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableView<?> tblEmployees;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewEmployeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
