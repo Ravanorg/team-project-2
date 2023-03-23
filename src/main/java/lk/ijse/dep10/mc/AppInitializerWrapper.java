344600b796d2deb79774f23f05832936159104de
Vishwa-Dinath <vishwadinath97@gmail.com>
2023-03-23 18:24:06 +0530

    Create UI and Established Database Connection

---
+++ src/main/java/lk/ijse/dep10/mc/AppInitializerWrapper.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep10.mc;
+
+public class AppInitializerWrapper {
+}
---
+++ src/main/java/lk/ijse/dep10/mc/controller/StudentViewController.java
@@ -0,0 +1,41 @@
+package lk.ijse.dep10.mc.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentViewController {
+
+    @FXML
+    private Button btnDelete;
+    @FXML
+    private Button btnNew;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private TableView<?> tblStudents;
+    @FXML
+    private TextField txtAddress;
+    @FXML
+    private TextField txtID;
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
